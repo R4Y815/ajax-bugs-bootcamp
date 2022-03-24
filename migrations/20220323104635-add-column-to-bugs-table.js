5454d90e872b79a257183e7879e607f70619c569
R4Y815 <Akcent360@gmail.com>
2022-03-25 07:41:20 +0800

    removed files beyond scope of this ICE.

--- migrations/20220323104635-add-column-to-bugs-table.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  async up(queryInterface, Sequelize) {
-    await queryInterface.addColumn('bugs', 'feature_id', { type: Sequelize.INTEGER });
-  },
-
-  async down(queryInterface) {
-    await queryInterface.removeColumn('bugs', 'feature_id');
-  },
-};
--- models/index.mjs
+++ models/index.mjs
@@ -2,7 +2,6 @@ import { Sequelize } from 'sequelize';
 import allConfig from '../config/config.js';
 
 import bugModel from './bug.mjs';
-import featureModel from './feature.mjs';
 
 const env = process.env.NODE_ENV || 'development';
 
@@ -19,11 +18,6 @@ let sequelize = new Sequelize(
 
 // add your model definitions to db here
 db.Bug = bugModel(sequelize, Sequelize.DataTypes);
-db.Feature = featureModel(sequelize, Sequelize.DataTypes);
-
-/* DEFINE FEATURES AND BUGS DB RELATIONSHIP HERE */
-db.Bug.belongsTo(db.Feature);
-db.Feature.hasMany(db.Bug);
 
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
